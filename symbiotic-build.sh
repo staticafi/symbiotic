62a95e807a6e2143377961b004a808bcecd75e3f
Marek Chalupa <mchqwerty@gmail.com>
2014-12-30 20:15:58 +0100

    allow start script from different positions
    When user wants to skip compiling llvm or slicer or whatever, he can :)

--- symbiotic-build.sh
+++ symbiotic-build.sh
@@ -3,9 +3,57 @@
 # Build symbiotic from scratch and setup environment for
 # development if needed
 
-# this is either 'shell' - that means setting development enivironment
-# and run shell - or it contains options for compilation
-OPTS="$1"
+export PREFIX=`pwd`/install
+export SYMBIOTIC_ENV=1
+
+FROM=
+case $1 in
+	'shell')
+		# stp needs this
+		ulimit -s unlimited
+
+		# most of the environment is already set
+		export PATH=$PREFIX/bin:$PATH
+
+		exec $SHELL
+	;;
+	'help')
+		echo "$0 [shell | slicer | svc13 | stp | klee | bin] OPTS"
+		echo -e "\tshell - run shell with environment set"
+		echo -e "\tslicer, svc13, stp"
+		echo -e "\tklee, bin           - run compilation _from_ this point"
+		echo -e "\t                      (dependencies exclusive)"
+		echo -e "\tOPTS = options for make (i. e. -j8)"
+		exit 0
+	;;
+	'slicer')
+		FROM='1'
+	;;
+	'svc13')
+		FROM='2'
+	;;
+	'stp')
+		FROM='3'
+	;;
+	'klee')
+		FROM='4'
+	;;
+	'bin')
+		FROM='5'
+	;;
+	*)
+		FROM='0'
+		OPTS="$1"
+	;;
+esac
+
+if [ ! -z "$FROM" ]; then
+	OPTS="$2"
+fi
+
+if [ "x$OPTS" = "x" ]; then
+	OPTS='-j1'
+fi
 
 check()
 {
@@ -35,40 +83,8 @@ check()
 	fi
 }
 
-set_env()
-{
-	export PREFIX=`pwd`/install
-	export SYMBIOTIC_ENV=1
-}
-
-set_env
-
-if [ "$OPTS" = "shell" ]; then
-	# stp needs this
-	ulimit -s unlimited
-
-	# most of the environment is already set
-	export PATH=$PREFIX/bin:$PATH
-
-	exec $SHELL
-elif [ "x$OPTS" = "x" ]; then
-	OPTS='-j1'
-fi
-
 # check if we have everything we need
 check
-
-# download llvm-3.2 and unpack
-if [ ! -d 'llvm-3.2.src' ]; then
-	wget http://llvm.org/releases/3.2/llvm-3.2.src.tar.gz || exit 1
-	wget http://llvm.org/releases/3.2/clang-3.2.src.tar.gz || exit 1
-
-	tar -xf llvm-3.2.src.tar.gz || exit 1
-	tar -xf clang-3.2.src.tar.gz || exit 1
-
-	# move clang to llvm/tools and rename to clang
-	mv clang-3.2.src llvm-3.2.src/tools/clang
-fi
 
 clean_and_exit()
 {
@@ -83,117 +99,140 @@ clean_and_exit()
 	exit $CODE
 }
 
-mkdir -p llvm-build-cmake
-cd llvm-build-cmake
+# download llvm-3.2 and unpack
+if [ $FROM -eq 0 ]; then
+	if [ ! -d 'llvm-3.2.src' ]; then
+		wget http://llvm.org/releases/3.2/llvm-3.2.src.tar.gz || exit 1
+		wget http://llvm.org/releases/3.2/clang-3.2.src.tar.gz || exit 1
 
-# configure llvm
-if [ ! -d CMakeFiles ]; then
-	cmake ../llvm-3.2.src \
-		-DCMAKE_BUILD_TYPE=Debug \
-		-DLLVM_INCLUDE_EXAMPLES=OFF \
-		-DLLVM_INCLUDE_TESTS=OFF \
-		-DLLVM_ENABLE_TIMESTAMPS=OFF \
-		-DLLVM_TARGETS_TO_BUILD="X86" \
-		-DCMAKE_C_FLAGS_DEBUG="-O0 -g" \
-		-DCMAKE_CXX_FLAGS_DEBUG="-O0 -g" || clean_and_exit
+		tar -xf llvm-3.2.src.tar.gz || exit 1
+		tar -xf clang-3.2.src.tar.gz || exit 1
+
+		# move clang to llvm/tools and rename to clang
+		mv clang-3.2.src llvm-3.2.src/tools/clang
+	fi
+
+	mkdir -p llvm-build-cmake
+	cd llvm-build-cmake
+
+	# configure llvm
+	if [ ! -d CMakeFiles ]; then
+		cmake ../llvm-3.2.src \
+			-DCMAKE_BUILD_TYPE=Debug \
+			-DLLVM_INCLUDE_EXAMPLES=OFF \
+			-DLLVM_INCLUDE_TESTS=OFF \
+			-DLLVM_ENABLE_TIMESTAMPS=OFF \
+			-DLLVM_TARGETS_TO_BUILD="X86" \
+			-DCMAKE_C_FLAGS_DEBUG="-O0 -g" \
+			-DCMAKE_CXX_FLAGS_DEBUG="-O0 -g" || clean_and_exit
+	fi
+
+	# build llvm
+	make "$OPTS" || exit 1
+
+	# we need build binaries
+
+	# we need these binaries in symbiotic
+	mkdir -p $PREFIX/bin
+	cp bin/clang $PREFIX/bin/clang || exit 1
+	cp bin/opt $PREFIX/bin/opt || exit 1
+	cd -
 fi
-
-# build llvm
-make "$OPTS" || exit 1
-
-# we need build binaries
-
-# we need these binaries in symbiotic
-mkdir -p $PREFIX/bin
-cp bin/clang $PREFIX/bin/ || exit 1
-cp bin/opt $PREFIX/bin/ || exit 1
-cd -
 
 export LLVM_DIR=`pwd`/llvm-build-cmake/share/llvm/cmake/
 
-rm -f llvm-3.2.src.tar.gz || exit 1
-rm -f clang-3.2.src.tar.gz || exit 1
+rm -f llvm-3.2.src.tar.gz &>/dev/null || exit 1
+rm -f clang-3.2.src.tar.gz &>/dev/null || exit 1
 
-# download slicer
-git clone git://github.com/jirislaby/LLVMSlicer.git
-cd LLVMSlicer
-if [ ! -d CMakeFiles ]; then
-	cmake . \
-		-DLLVM_SRC_PATH=../llvm-3.2.src/ \
-		-DLLVM_BUILD_PATH=../llvm-build-cmake/ \
-		-DCMAKE_INSTALL_PREFIX=$PREFIX || clean_and_exit 1 "git"
+if [ $FROM -le 1 ]; then
+	# download slicer
+	git clone git://github.com/jirislaby/LLVMSlicer.git
+	cd LLVMSlicer
+	if [ ! -d CMakeFiles ]; then
+		cmake . \
+			-DLLVM_SRC_PATH=../llvm-3.2.src/ \
+			-DLLVM_BUILD_PATH=../llvm-build-cmake/ \
+			-DCMAKE_INSTALL_PREFIX=$PREFIX || clean_and_exit 1 "git"
+	fi
+
+	(make "$OPTS" && make install) || exit 1
+	cd -
 fi
 
-(make "$OPTS" && make install) || exit 1
-cd -
 
+if [ $FROM -le 2 ]; then
+	# download svc13
+	git clone git://github.com/jirislaby/svc13.git
+	cd svc13
+	if [ ! -d CMakeFiles ]; then
+		cmake . \
+			-DLLVM_SRC_PATH=../llvm-3.2.src/ \
+			-DLLVM_BUILD_PATH=../llvm-build-cmake/ \
+			-DCMAKE_INSTALL_PREFIX=$PREFIX || clean_and_exit 1 "git"
+	fi
 
-# download svc13
-git clone git://github.com/jirislaby/svc13.git
-cd svc13
-if [ ! -d CMakeFiles ]; then
-	cmake . \
-		-DLLVM_SRC_PATH=../llvm-3.2.src/ \
-		-DLLVM_BUILD_PATH=../llvm-build-cmake/ \
-		-DCMAKE_INSTALL_PREFIX=$PREFIX || clean_and_exit 1 "git"
+	(make "$OPTS" && make install) || exit 1
+	cd -
 fi
 
-(make "$OPTS" && make install) || exit 1
-cd -
+if [ $FROM -le 3 ]; then
+	git clone git://github.com/stp/stp.git
+	cd stp
+	cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX \
+		-DBUILD_SHARED_LIBS:BOOL=OFF \
+		-DENABLE_PYTHON_INTERFACE:BOOL=OFF || clean_and_exit 1 "git"
 
-git clone git://github.com/stp/stp.git
-cd stp
-cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX \
-	-DBUILD_SHARED_LIBS:BOOL=OFF \
-	-DENABLE_PYTHON_INTERFACE:BOOL=OFF || clean_and_exit 1 "git"
+	(make "$OPTS" OPTIMIZE=-O2 CFLAGS_M32=install && make install) || exit 1
+	cd -
 
-(make "$OPTS" OPTIMIZE=-O2 CFLAGS_M32=install && make install) || exit 1
-cd -
+	# we must build llvm once again with configure script (klee needs this)
+	mkdir -p llvm-build-configure
+	cd llvm-build-configure
 
-# we must build llvm once again with configure script (klee needs this)
-mkdir -p llvm-build-configure
-cd llvm-build-configure
+	# configure llvm if not done yet
+	if [ ! -f config.log ]; then
+		../llvm-3.2.src/configure \
+			--enable-optimized --enable-assertions \
+			--enable-targets=x86 --enable-docs=no || clean_and_exit 1
+	fi
 
-# configure llvm if not done yet
-if [ ! -f config.log ]; then
-	../llvm-3.2.src/configure \
-		--enable-optimized --enable-assertions \
-		--enable-targets=x86 --enable-docs=no || clean_and_exit 1
+	make "$OPTS" || exit 1
+	cd -
 fi
 
-make "$OPTS" || exit 1
-cd -
 
+if [ $FROM -le 4 ]; then
+	# build klee
+	git clone git://github.com/klee/klee.git
+	cd klee
 
-# build klee
-git clone git://github.com/klee/klee.git
-cd klee
-
-if [ ! -f config.log ]; then
+	if [ ! -f config.log ]; then
 	./configure \
 		--prefix=$PREFIX \
 		--with-llvmsrc=../llvm-3.2.src \
 		--with-llvmobj=../llvm-build-configure \
 		--with-stp=$PREFIX || clean_and_exit 1 "git"
+	fi
+
+	(make "$OPTS" ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 ENABLE_SHARED=0 && make install) || exit 1
+	cd -
 fi
 
-(make "$OPTS" ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 ENABLE_SHARED=0 && make install) || exit 1
-cd -
+if [ $FROM -le 5 ]; then
+	cd $PREFIX
+	# create git repository and add all files that we need
+	# then remove the rest and create distribution
+	git init
+	git add \
+		bin/clang \
+		bin/opt \
+		bin/klee \
+		lib/LLVMSlicer.so \
+		lib/LLVMsvc13.so \
+		lib/libkleeRuntest.so \
+		lib/libkleeRuntimeIntrinsic.bca
 
-cd $PREFIX
-# create git repository and add all files that we need
-# then remove the rest and create distribution
-git init
-git add \
-	bin/clang \
-	bin/opt \
-	bin/klee \
-	bin/stp \
-	lib/LLVMSlicer.so \
-	lib/LLVMsvc13.so \
-	lib/libkleeRuntest.so \
-	lib/libkleeRuntimeIntrinsic.bca
-
-git commit -m "Create Symbiotic distribution `date`"
-# remove unnecessary files
-git clean -xdf
+	git commit -m "Create Symbiotic distribution `date`"
+	# remove unnecessary files
+	git clean -xdf
+fi
