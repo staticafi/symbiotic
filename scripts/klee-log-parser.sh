707b8f8a9dbdc5c87667266009a0da16e2e13435
Statica project <statica@fi.muni.cz>
2015-09-02 16:46:03 +0200

    update klee-log-parser.h
    add two new errors

--- scripts/klee-log-parser.sh
+++ scripts/klee-log-parser.sh
@@ -8,7 +8,7 @@ fi
 
 for file in $FILES; do
 #	grep -q 'LastChar > FirstChar' $file && echo -n X
-	grep -q 'ASSERTION FAIL' $file && echo -n 'ASSERTIONFAILED '
+	grep -q 'ASSERTION FAIL: verifier assertion failed' $file && echo -n 'ASSERTIONFAILED '
 	grep -q 'query timed out (resolve)' $file && echo -n 'ESTPTIMEOUT '
 	grep -q 'HaltTimer invoked' $file && echo -n 'EKLEETIMEOUT '
 	grep -q 'failed external call' $file && echo -n 'EEXTERNCALL '
@@ -16,6 +16,8 @@ for file in $FILES; do
 #	grep -qE 'memory error|invalid function pointer' $file && echo -n 'EINVALPTR '
 	grep -q 'LLVM ERROR: Code generator does not support' $file && echo -n 'EINVALINST '
 	grep -q 'klee: .*Assertion .* failed.' $file && echo -n 'EKLEEASSERT '
+	grep -q 'unable to compute initial values' $file && echo -n 'EKLEEERROR '
+	grep -q ' unable to get symbolic solution' $file && echo -n 'EKLEEERROR '
 	grep -q 'silently concretizing'  $file && echo -n 'ESILENTLYCONCRETIZED '
 	grep -q 'calling .* with extra arguments'  $file && echo -n 'EEXTRAARGS '
 	grep -q 'abort failure'  $file && echo -n 'EABORT '
