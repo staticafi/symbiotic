18bd5efdc408eb321a0c47e6a28458a4f2dd2eeb
Statica project <statica@fi.muni.cz>
2015-10-21 20:11:13 +0200

    verify_path: some fixes

--- scripts/verify_path.sh
+++ scripts/verify_path.sh
@@ -1,16 +1,20 @@
 DIR=`dirname $0`
 
+#FIXME move this script to satt,
+# it has nothing in common with this repository
 
 GRAPHML="$1"
 PRPFILE="$2"
 BENCHMARK="$3"
+VERIFIER_RESULT="$GRAPHML.result"
+
 BENCHMARKDIR=`dirname "$BENCHMARK"`
 
 test -z $CPA && CPA="$DIR/CPAchecker/scripts/cpa.sh"
 CPADIR=`dirname $CPA`
 
 cd $CPADIR/.. || exit 1
-STATUS=$(scripts/cpa.sh -noout -heap 10000M -predicateAnalysis \
+(scripts/cpa.sh -noout -heap 10000M -predicateAnalysis \
 -setprop cfa.useMultiEdges=false \
 -setprop cpa.predicate.solver=MATHSAT5 \
 -setprop cfa.simplifyCfa=false \
@@ -34,12 +38,19 @@ STATUS=$(scripts/cpa.sh -noout -heap 10000M -predicateAnalysis \
 -skipRecursion \
 -spec "$GRAPHML" \
 -spec "$PRPFILE" \
-"$BENCHMARK" 2>&1 | if grep -q 'Error path found and confirmed'; then echo "confirmed"; fi)
+"$BENCHMARK") 1>/dev/null 2>"$VERIFIER_RESULT"
 
 if [ $? -ne 0 ]; then
-	STATUS="error"
-elif [ "$STATUS" != "confirmed" ]; then
-	STATUS="unconfirmed"
+	echo "error"
+elif grep -q 'Error path found and confirmed' "$VERIFIER_RESULT"; then
+	echo "confirmed"
+else
+	echo "unconfirmed"
 fi
 
-echo "$STATUS"
+echo "=== WITNESS OUTPUT"
+cat "$VERIFIER_RESULT"
+echo "--- WITNESS ---"
+cat "$GRAPHML"
+
+rm "$VERIFIER_RESULT"
