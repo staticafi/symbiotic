373912c1bee1633efaebce4aed2c474b092d8fd9
Statica group <statica@fi.muni.cz>
2016-09-02 09:53:24 +0200

    fix symbiotic.utils.__init__.py
    forgot to add enable_debug func

--- lib/symbioticpy/symbiotic/utils/__init__.py
+++ lib/symbioticpy/symbiotic/utils/__init__.py
@@ -1,3 +1,3 @@
 
-from utils import err, dbg, print_elapsed_time, restart_counting_time
+from utils import err, dbg, enable_debug, print_elapsed_time, restart_counting_time
 from timeout import start_timeout, stop_timeout, Timeout
