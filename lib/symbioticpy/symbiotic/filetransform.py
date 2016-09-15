5e2910b3ce129973a1c1d198810e482643d99685
Statica group <statica@fi.muni.cz>
2016-09-15 15:06:21 +0200

    remove __inline from sources
    clang still has the bug that corrupts the code when
    a function has __inline

--- lib/symbioticpy/symbiotic/filetransform.py
+++ lib/symbioticpy/symbiotic/filetransform.py
@@ -9,16 +9,42 @@ class FileTransform(object):
     def run(self, inputfile, outputfile):
         pass
 
-class TypedefReplace(FileTransform):
+#class TypedefReplace(FileTransform):
+#   """
+#   Replace typedef by the original type in a C file
+#   """
+#   def __init__(self):
+#       self._typedefs = {}
+#
+#       from re import compile
+#       # FIXME: this does not match everything
+#       self._tdre = compile('^\s*typedef\s+(.*)\s+(\w+)\s*;\s*$')
+#
+#   def run(self, inputfile, outputfile):
+#       infile = open(inputfile, 'r')
+#       outfile = open(outputfile, 'w')
+#
+#       for l in infile:
+#           res = self._tdre.match(l)
+#           if res:
+#               v = res.group(1).strip()
+#               k = res.group(2).strip()
+#               assert not self._typedefs.has_key(k)
+#               self._typedefs[k] = v
+#               outfile.write('/* {0} -> {1} */\n'.format(res.group(1), res.group(2).strip()))
+#           else:
+#               for (k, v) in self._typedefs.iteritems():
+#                   l = l.replace(k, v)
+#
+#               outfile.write(l)
+
+class InlineRemove(FileTransform):
     """
-    Replace typedef by the original type in a C file
+    Remove __inline due to a bug in clang
     """
     def __init__(self):
-        self._typedefs = {}
-
         from re import compile
-        # FIXME: this does not match everything
-        self._tdre = compile('^\s*typedef\s+(.*)\s+(\w+)\s*;\s*$')
+        self._tdre = compile('^\s*__inline\s+(.*)$')
 
     def run(self, inputfile, outputfile):
         infile = open(inputfile, 'r')
@@ -27,14 +53,8 @@ class TypedefReplace(FileTransform):
         for l in infile:
             res = self._tdre.match(l)
             if res:
-                v = res.group(1).strip()
-                k = res.group(2).strip()
-                assert not self._typedefs.has_key(k)
-                self._typedefs[k] = v
-                outfile.write('/* {0} -> {1} */\n'.format(res.group(1), res.group(2).strip()))
+                outfile.write('/*__inline */ {0}\n'.format(res.group(1)))
             else:
-                for (k, v) in self._typedefs.iteritems():
-                    l = l.replace(k, v)
-
                 outfile.write(l)
+
 
