3deed40e4a42bbaf6216c1093ed55caecf862909
Marek Chalupa <mchqwerty@gmail.com>
2016-10-28 10:59:15 +0200

    simplify some nondet calls
    a lot of benchmarks uses __VERIFIER_nondet_int() as
    __VERIFIER_nondet_bool(), which redundantly makes larger
    bitvectors. Do simple text replacement when found such call.
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- lib/symbioticpy/symbiotic/filetransform.py
+++ lib/symbioticpy/symbiotic/filetransform.py
@@ -9,35 +9,6 @@ class FileTransform(object):
     def run(self, inputfile, outputfile):
         pass
 
-#class TypedefReplace(FileTransform):
-#   """
-#   Replace typedef by the original type in a C file
-#   """
-#   def __init__(self):
-#       self._typedefs = {}
-#
-#       from re import compile
-#       # FIXME: this does not match everything
-#       self._tdre = compile('^\s*typedef\s+(.*)\s+(\w+)\s*;\s*$')
-#
-#   def run(self, inputfile, outputfile):
-#       infile = open(inputfile, 'r')
-#       outfile = open(outputfile, 'w')
-#
-#       for l in infile:
-#           res = self._tdre.match(l)
-#           if res:
-#               v = res.group(1).strip()
-#               k = res.group(2).strip()
-#               assert not self._typedefs.has_key(k)
-#               self._typedefs[k] = v
-#               outfile.write('/* {0} -> {1} */\n'.format(res.group(1), res.group(2).strip()))
-#           else:
-#               for (k, v) in self._typedefs.iteritems():
-#                   l = l.replace(k, v)
-#
-#               outfile.write(l)
-
 class InlineRemove(FileTransform):
     """
     Remove __inline due to a bug in clang
@@ -57,4 +28,31 @@ class InlineRemove(FileTransform):
             else:
                 outfile.write(l)
 
+class NondetSimplify(FileTransform):
+    """
+    Simplify some nondeterministic calls
+    """
+    def __init__(self):
+        from re import compile
+        self._re1 = compile('^(\s*)if\s*\(\s*__VERIFIER_nondet_(int|uint)\(\s*\)\s*\)(.*)$')
+        self._re2 = compile('^(\s*)while\s*\(\s*__VERIFIER_nondet_(int|uint)\(\s*\)\s*\)(.*)$')
+
+    def run(self, inputfile, outputfile):
+        infile = open(inputfile, 'r')
+        outfile = open(outputfile, 'w')
+
+        outfile.write('_Bool __VERIFIER_nondet__Bool(void);\n\n')
+
+        for l in infile:
+            res = self._re1.match(l)
+            keyword = 'if'
+            if not res:
+                res = self._re2.match(l)
+                keyword = 'while'
+
+            if res:
+                outfile.write('{0}/* {1} -> bool */\n'.format(res.group(1), res.group(2)))
+                outfile.write('{0}{1} (__VERIFIER_nondet__Bool()){2}\n'.format(res.group(1), keyword, res.group(3)))
+            else:
+                outfile.write(l)
 
