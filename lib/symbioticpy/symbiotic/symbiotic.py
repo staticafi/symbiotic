23a107cdb4a21830e5f2743312feeab91c9e3e8c
Marek Chalupa <mchqwerty@gmail.com>
2016-11-16 17:47:14 +0100

    slice even with memsafety properties
    we instrument the code, so now we know where to slice
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- lib/symbioticpy/symbiotic/symbiotic.py
+++ lib/symbioticpy/symbiotic/symbiotic.py
@@ -592,12 +592,7 @@ class Symbiotic(object):
                 self.remove_unused_only()
 
         # optimize the code before symbolic execution
-	# but only iff we're not looking for memsafety
-	# (because the optimization could remove the errors)
-        if 'VALID-DEREF' in self.options.prp:
-	    opt = []
-        else:
-	    opt = get_optlist_after(self.options.optlevel)
+        opt = get_optlist_after(self.options.optlevel)
         if opt:
             self.optimize(passes=opt)
 
