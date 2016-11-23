3d64c4710878caede0601192da621ccf6c7e9355
Marek Chalupa <mchqwerty@gmail.com>
2016-11-23 19:23:37 +0100

    lib/kzalloc.c: fix prototype
    name the parameter

--- lib/kzalloc.c
+++ lib/kzalloc.c
@@ -1,9 +1,11 @@
 #include "symbiotic-size_t.h"
 
-void *kzalloc(int size, int)
+void *kzalloc(int size, int flgs)
 {
+	(void) flgs;
 	extern void *malloc(size_t size);
 	void *mem = malloc(size);
 	memset(mem, 0, size);
+
 	return mem;
 }
