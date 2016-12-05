d4578b4eaf306008ff3a9d1efb76ea57559fb618
Marek Chalupa <mchqwerty@gmail.com>
2016-12-05 20:14:11 +0100

    fix prototype of kmalloc
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- lib/kernel/__kmalloc.c
+++ lib/kernel/__kmalloc.c
@@ -1,7 +1,8 @@
 #include "symbiotic-size_t.h"
 
-void *__kmalloc(size_t size)
+void *__kmalloc(size_t size, unsigned flags)
 {
+	(void) flags;
 	extern void *malloc(size_t);
 	return malloc(size);
 }
--- lib/kernel/kmalloc.c
+++ lib/kernel/kmalloc.c
@@ -2,7 +2,8 @@
 
 extern void *malloc(size_t);
 
-void *kmalloc(size_t size)
+void *kmalloc(size_t size, unsigned flags)
 {
+	(void) flags;
 	return malloc(size);
 }
