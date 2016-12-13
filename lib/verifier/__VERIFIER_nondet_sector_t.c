4d7e5fdb2f1dcdeb1ad9cb5b648659a6c1f37ec1
Marek Chalupa <mchalupa@mail.muni.cz>
2016-12-13 15:32:34 +0100

    add __VERIFIER_nondet_sector_t.c
    alias for ulong
    
    Signed-off-by: Marek Chalupa <mchalupa@mail.muni.cz>

---
+++ lib/verifier/__VERIFIER_nondet_sector_t.c
@@ -0,0 +1,10 @@
+#include "symbiotic-size_t.h"
+
+extern void klee_make_symbolic(void *, size_t, const char *);
+
+unsigned long __VERIFIER_nondet_sector_t(void)
+{
+	unsigned long x;
+	klee_make_symbolic(&x, sizeof(x), "nondet-sector_t");
+	return x;
+}
