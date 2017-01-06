bda4de12f321db97c06cd9fb8e35090db07d21dc
Marek Chalupa <mchalupa@mail.muni.cz>
2017-01-06 16:34:43 +0100

    use automatic inference of size in klee_make_symbolic

--- lib/verifier/__VERIFIER_malloc0.c
+++ lib/verifier/__VERIFIER_malloc0.c
@@ -10,7 +10,7 @@ void *__VERIFIER_malloc0(size_t size)
 	void *mem = malloc(size);
 	// NOTE: klee already assumes that
 	//klee_assume(mem != (void *) 0);
-	klee_make_symbolic(mem, size, "malloc0");
+	klee_make_symbolic(mem, 0, "malloc0");
 
 	return mem;
 }
