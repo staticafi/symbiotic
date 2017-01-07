4eed46841a8fc8e884545216ba957ce9c9c6d6c1
Marek Chalupa <mchalupa@mail.muni.cz>
2017-01-07 17:19:35 +0100

    make __VERIFIER_exit an abort()

--- lib/verifier/__VERIFIER_exit.c
+++ lib/verifier/__VERIFIER_exit.c
@@ -1,5 +1,7 @@
-extern void exit(int);
+//extern void exit(int);
+extern void abort(void) __attribute__((noreturn));
 void __VERIFIER_exit(int status) __attribute__((noinline));
 void __VERIFIER_exit(int status) {
-	exit(status);
+//	exit(status);
+	abort();
 }
