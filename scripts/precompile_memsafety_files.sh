10cc1adcfec99704f1456bdd342aa40ce2e3e30c
Marek Chalupa <xchalup4@fi.muni.cz>
2017-02-01 09:03:15 +0100

    scripts/precompile_memsafety_files.sh: work with different versions of llvm
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- scripts/precompile_memsafety_files.sh
+++ scripts/precompile_memsafety_files.sh
@@ -1,6 +1,5 @@
 INSTR="LLVMInstrumentation/"
 PREFIX="install"
-CLANG=$PREFIX/bin/clang
 
 set -e
 
@@ -10,14 +9,19 @@ if [ ! -f "scripts/`basename $0`" ]; then
 fi
 
 FILES=
-for F in `find $INSTR/instrumentations/ -name '*.c'`; do
-	NAME=`basename $F`
-	OUT=${NAME%*.c}.bc
-	FILES="$FILES lib/$OUT"
-	$CLANG -emit-llvm -c $F -o $PREFIX/lib/$OUT $CPPFLAGS $CFLAGS $LDFLAGS
+for LLVM in $PREFIX/llvm-*; do
+	CLANG=$LLVM/bin/clang
+	for F in `find $INSTR/instrumentations/ -name '*.c'`; do
+		NAME=`basename $F`
+		OUT=${NAME%*.c}.bc
+		mkdir -p "$LLVM/lib" "$LLVM/lib32"
 
-	FILES="$FILES lib32/$OUT"
-	$CLANG -emit-llvm -c $F -m32 -o $PREFIX/lib32/$OUT $CPPFLAGS $CFLAGS $LDFLAGS
+		FILES="$FILES ${LLVM#install/}/lib/$OUT"
+		$CLANG -emit-llvm -c $F -o $LLVM/lib/$OUT $CPPFLAGS $CFLAGS $LDFLAGS
+
+		FILES="$FILES ${LLVM#install/}/lib32/$OUT"
+		$CLANG -emit-llvm -c $F -m32 -o $LLVM/lib32/$OUT $CPPFLAGS $CFLAGS $LDFLAGS
+	done
 done
 
 echo "To add precompiled files to distribution, run this command from install/ folder:"
