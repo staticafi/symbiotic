e94938030809bc82d1608989a9821e86f5884c09
Marek Chalupa <xchalup4@fi.muni.cz>
2017-02-02 13:04:51 +0100

    add -replace-asserts
    This replaces asserts with __VERIFIER_error. It is needed
    for other tools than KLEE
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- src/ReplaceUBSan.cpp
+++ src/ReplaceUBSan.cpp
@@ -162,3 +162,62 @@ static RegisterPass<RemoveErrorCalls> RERC("remove-error-calls",
                                            "Remove calls to __VERIFIER_error");
 char RemoveErrorCalls::ID;
 
+
+class ReplaceAsserts : public FunctionPass {
+  public:
+    static char ID;
+
+    ReplaceAsserts() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F);
+};
+
+bool ReplaceAsserts::runOnFunction(Function &F)
+{
+  bool modified = false;
+  Module *M = F.getParent();
+  Constant *ver_err = nullptr;
+
+  for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E;) {
+    Instruction *ins = &*I;
+    ++I;
+    if (CallInst *CI = dyn_cast<CallInst>(ins)) {
+      if (CI->isInlineAsm())
+        continue;
+
+      const Value *val = CI->getCalledValue()->stripPointerCasts();
+      const Function *callee = dyn_cast<Function>(val);
+      if (!callee || callee->isIntrinsic())
+        continue;
+
+      if (!callee->isDeclaration())
+        continue;
+
+      assert(callee->hasName());
+      StringRef name = callee->getName();
+      if (!name.equals("__assert_fail"))
+        continue;
+
+      if (!ver_err) {
+        LLVMContext& Ctx = M->getContext();
+        ver_err = M->getOrInsertFunction("__VERIFIER_error",
+                                         Type::getVoidTy(Ctx),
+                                         nullptr);
+      }
+
+      auto CI2 = CallInst::Create(ver_err);
+      CallAddMetadata(CI2, CI);
+
+      CI2->insertAfter(CI);
+      CI->eraseFromParent();
+
+      modified = true;
+    }
+  }
+  return modified;
+}
+
+static RegisterPass<ReplaceAsserts> RASS("replace-asserts",
+                                         "Replace assert calls with calls to __VERIFIER_error");
+char ReplaceAsserts::ID;
+
