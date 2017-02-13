e03e309be002f669ae7320323e0ad4761124e466
Marek Chalupa <xchalup4@fi.muni.cz>
2017-02-13 10:41:29 +0100

    lib: don't try to do symbolic the objects of 0 size
    when the size is 0, the returned pointer may not point
    to a valid object
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/verifier/__VERIFIER_malloc0.c
+++ lib/verifier/__VERIFIER_malloc0.c
@@ -10,7 +10,8 @@ void *__VERIFIER_malloc0(size_t size)
 	void *mem = malloc(size);
 	// NOTE: klee already assumes that
 	//klee_assume(mem != (void *) 0);
-	klee_make_symbolic(mem, 0, "malloc0");
+	if (size > 0)
+		klee_make_symbolic(mem, 0, "malloc0");
 
 	return mem;
 }
