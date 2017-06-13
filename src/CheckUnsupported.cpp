685d3a9713b1e20b6d51db8a1161c58c01260245
Jiri Slaby <slaby@fi.muni.cz>
2017-06-13 09:59:31 +0200

    llvm4: handle #if's
    Signed-off-by: Jiri Slaby <slaby@fi.muni.cz>

--- src/CheckUnsupported.cpp
+++ src/CheckUnsupported.cpp
@@ -16,7 +16,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/TypeBuilder.h"
-#if (LLVM_VERSION_MINOR >= 5)
+#if LLVM_VERSION_MAJOR >= 4 || (LLVM_VERSION_MAJOR == 3 && LLVM_VERSION_MINOR >= 5)
   #include "llvm/IR/InstIterator.h"
 #else
   #include "llvm/Support/InstIterator.h"
