7b1e8abbd594c07ac36e85d777fbc76d87474e9e
Marek Chalupa <xchalup4@fi.muni.cz>
2017-10-17 12:23:11 +0200

    add CloneMetadata.cpp
    Factor out this function.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

---
+++ src/CloneMetadata.cpp
@@ -0,0 +1,52 @@
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+
+#include <cassert>
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/raw_ostream.h"
+#include <llvm/IR/DebugInfoMetadata.h>
+
+using namespace llvm;
+
+/** Clone metadata from one instruction to another.
+ * If i1 does not contain any metadata, then the instruction
+ * that is closest to i1 is picked (we prefer the one that is after
+ * and if there is none, then use the closest one before).
+ *
+ * @param i1 the first instruction
+ * @param i2 the second instruction without any metadata
+ */
+void CloneMetadata(const llvm::Instruction *i1, llvm::Instruction *i2)
+{
+    if (i1->hasMetadata()) {
+        i2->setDebugLoc(i1->getDebugLoc());
+        return;
+    }
+
+    const llvm::Instruction *metadataI = nullptr;
+    bool after = false;
+    for (const llvm::Instruction& I : *i1->getParent()) {
+        if (&I == i1) {
+            after = true;
+            continue;
+        }
+
+        if (I.hasMetadata()) {
+            // store every "last" instruction with metadata,
+            // so that in the case that we won't find anything
+            // after i1, we can use metadata that are the closest
+            // "before" i1
+            metadataI = &I;
+            if (after)
+                break;
+        }
+    }
+
+    assert(metadataI && "Did not find dbg in any instruction of a block");
+    i2->setDebugLoc(metadataI->getDebugLoc());
+}
+
+
