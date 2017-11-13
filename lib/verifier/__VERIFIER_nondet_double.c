05102c97b8b44ad2394e08af7653a66782fac9b9
Marek Chalupa <xchalup4@fi.muni.cz>
2017-11-13 15:43:43 +0100

    lib/verifier: make nondet floats return Nan too
    When the value is set as symbolic, it does not work
    well with NaN, which is kind of special.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/verifier/__VERIFIER_nondet_double.c
+++ lib/verifier/__VERIFIER_nondet_double.c
@@ -4,7 +4,13 @@ extern void klee_make_symbolic(void *, size_t, const char *);
 
 double __VERIFIER_nondet_double(void)
 {
-	double x;
-	klee_make_symbolic(&x, sizeof(x), "nondet-double");
-	return x;
+	_Bool nan_choice;
+	klee_make_symbolic(&nan_choice, sizeof(_Bool), "nondet_double_is_nan");
+	if (nan_choice) {
+		return ((double)0.0)/0.0;
+	} else {
+		double x;
+		klee_make_symbolic(&x, sizeof(x), "nondet-double");
+		return x;
+	}
 }
