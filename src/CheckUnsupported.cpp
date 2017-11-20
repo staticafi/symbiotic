a1a25e62bc6b44b098f769b0ad896187631a0140
Marek Chalupa <xchalup4@fi.muni.cz>
2017-11-20 14:39:09 +0100

    getopt32 is unsupported
    there's is a bug in our tool or in sv-comp benchmark, but
    I'm not sure where. Until I found out, do not support it.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- src/CheckUnsupported.cpp
+++ src/CheckUnsupported.cpp
@@ -107,6 +107,10 @@ bool CheckUnsupported::runOnFunction(Function &F) {
     "fdiml",
     "longjmp",
     "setjmp",
+    // I do not know if this is problem with us or with
+    // benchmarks, but until I found out,
+    // skip benchmarks with this function
+    "getopt32",
     nullptr
   };
 
