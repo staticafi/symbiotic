ff71159c6963c8f2d3a83d8fe720d942c1321894
Marek Chalupa <xchalup4@fi.muni.cz>
2017-11-27 18:20:31 +0100

    filetransform: fix working with python2

--- lib/symbioticpy/symbiotic/filetransform.py
+++ lib/symbioticpy/symbiotic/filetransform.py
@@ -28,49 +28,13 @@ class InlineRemove(FileTransform):
         for l in infile:
             res = self._tdre.match(l)
             if res:
-                outfile.write('/*{0} */ {1}\n'.format(res.group(1), res.group(2)))
+                line = u'/*{0} */ {1}\n'.format(res.group(1), res.group(2))
+                if version_info < (3,0):
+                    line = line.decode('utf-8')
             else:
-                outfile.write(l)
+                line = l
 
-class ReplaceNamedNondet(FileTransform):
-    """
-    Replace all nondet() calls to nondet_named(#__LINE__) calls
-    """
-    def __init__(self):
-        from re import compile
-        # XXX: this replaces also declarations -- and we do not have a namy anyway
-        #self._re = compile('(.*)__VERIFIER_nondet_(.*)\(\s*\)(.*)')
-        self._re2 = compile('(.*\s(.+)\s*=\s*)__VERIFIER_nondet_(.*)\(\s*\)(.*)')
-
-    def run(self, inputfile, outputfile):
-        infile = open(inputfile, 'r', encoding='utf-8')
-        outfile = open(outputfile, 'w', encoding='utf-8')
-
-        n = 1
-        for l in infile:
-            res = self._re2.match(l)
-            if res:
-                prefix = res.group(1)
-                name = res.group(2).rstrip()
-                ctype = res.group(3)
-                suffix = res.group(4)
-           #else:
-           #    # no luck, try without name
-           #    res = self._re.match(l)
-           #    if res:
-           #        prefix = res.group(1)
-           #        name = '--'
-           #        ctype = res.group(2)
-           #        suffix = res.group(3)
-
-            if res:
-                outfile.write('{0}__VERIFIER_nondet_{1}_named("{2}:{3}"){4}\n'.\
-                            format(prefix, ctype, name, n, suffix))
-            else:
-                outfile.write(l)
-
-            n += 1
-
+            outfile.write(line)
 
 class NondetSimplify(FileTransform):
     """
@@ -95,9 +59,14 @@ class NondetSimplify(FileTransform):
             if res:
                 # we do not want to change numbers because of witnesses
                 # outfile.write('{0}/* {1} -> bool */\n'.format(res.group(1), res.group(2)))
-                outfile.write('{0}{1} (__VERIFIER_nondet__Bool()){2}\n'.format(res.group(1), keyword, res.group(3)))
+                line = '{0}{1} (__VERIFIER_nondet__Bool()){2}\n'.format(res.group(1), keyword, res.group(3))
+                if version_info < (3,0):
+                    line = line.decode('utf-8')
             else:
-                outfile.write(l)
+                line = l
+
+            outfile.write(line)
+
 
 class InfiniteLoopsRemover(FileTransform):
     """
@@ -122,7 +91,10 @@ class InfiniteLoopsRemover(FileTransform):
                 if res:
                     # we do not want to change numbers because of witnesses
                     # outfile.write('{0}/* {1} -> bool */\n'.format(res.group(1), res.group(2)))
-                    outfile.write('{0}volatile _Bool inf_true{1} = 1; while(inf_true{1}){2}\n'.format(res.group(1), lines, res.group(2)))
+                    if version_info < (3,0):
+                        outfile.write('{0}volatile _Bool inf_true{1} = 1; while(inf_true{1}){2}\n'.format(res.group(1), lines, res.group(2)).decode('utf-8'))
+                    else:
+                        outfile.write('{0}volatile _Bool inf_true{1} = 1; while(inf_true{1}){2}\n'.format(res.group(1), lines, res.group(2)))
                     found = True
                     break
 
