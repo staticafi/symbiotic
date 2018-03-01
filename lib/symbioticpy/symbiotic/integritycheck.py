259d027b4359d9a9458407ea6865c59acc6bc3bf
Marek Chalupa <xchalup4@fi.muni.cz>
2018-03-01 11:37:14 +0100

    make integrity check compatible with python2
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/symbioticpy/symbiotic/integritycheck.py
+++ lib/symbioticpy/symbiotic/integritycheck.py
@@ -1,28 +1,27 @@
 from sys import version_info
-from symbiotic import SymbioticException
-from symbiotic.utils.utils import process_grep
-from symbiotic.utils.process import ProcessRunner
-from symbiotic.utils.watch import ProcessWatch
+from . exceptions import SymbioticException
+from . utils.utils import process_grep
+from . utils.process import ProcessRunner
+from . utils.watch import ProcessWatch
 
 class IntegrityChecker(object):
     def __init__(self, versions):
         self._versions = versions
 
-    def _decode(vers):
-        print('decoding', vers)
+    def _decode(self, vers):
         if version_info < (3,0):
             return vers.decode('utf-8')
         else:
             return bytes(vers, 'utf-8')
 
-    def get_klee_version():
+    def _get_klee_version(self):
         (retval, lines) = process_grep(['klee', '-version'], 'revision')
         assert retval == 0
         assert(len(lines) == 1)
 
         return lines[0].split(b':')[1].strip()
 
-    def get_slicer_version():
+    def _get_slicer_version(self):
         pr = ProcessRunner(['sbt-slicer', '-version'], ProcessWatch(1))
         retval = pr.run()
         assert retval == 0
@@ -31,7 +30,7 @@ class IntegrityChecker(object):
 
         return lines[0].strip()[:8]
 
-    def get_instr_version():
+    def _get_instr_version(self):
         pr = ProcessRunner(['sbt-instr', '--version'], ProcessWatch(1))
         retval = pr.run()
         assert retval == 0
@@ -40,7 +39,7 @@ class IntegrityChecker(object):
 
         return lines[0].strip()
 
-    def _check(component, expected_version, actual_version):
+    def _check(self, component, expected_version, actual_version):
         if expected_version != actual_version:
             raise SymbioticException("The version of '{0}' is different than expected (expect {1} but got {2})".\
                                      format(component, expected_version, actual_version))
@@ -53,15 +52,15 @@ class IntegrityChecker(object):
         for (k, v) in self._versions.items():
             #print(k,v)
             if k == 'KLEE':
-                vers = IntegrityChecker.get_klee_version()
-                expected = IntegrityChecker._decode(v)
-                IntegrityChecker._check(k, expected, vers)
+                vers = self._get_klee_version()
+                expected = self._decode(v)
+                self._check(k, expected, vers)
             elif k == 'sbt-slicer':
-                vers = IntegrityChecker.get_slicer_version()
-                expected = IntegrityChecker._decode(v)
-                IntegrityChecker._check(k, expected, vers)
+                vers = self._get_slicer_version()
+                expected = self._decode(v)
+                self._check(k, expected, vers)
             elif k == 'sbt-instrumentation':
-                vers = IntegrityChecker.get_instr_version()
-                expected = IntegrityChecker._decode(v[:8])
-                IntegrityChecker._check(k, expected, vers)
+                vers = self._get_instr_version()
+                expected = self._decode(v[:8])
+                self._check(k, expected, vers)
 
