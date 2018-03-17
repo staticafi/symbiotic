5cd191dcc313ecd807a1c1e3d0b468627d399afc
Marek Chalupa <xchalup4@fi.muni.cz>
2018-03-17 13:37:10 +0100

    split 'verifier' funs to 'svcomp' and 'verifier'
    Some are svcomp specific and some are generic for verifier.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/symbioticpy/symbiotic/options.py
+++ lib/symbioticpy/symbiotic/options.py
@@ -24,7 +24,7 @@ class SymbioticOptions(object):
         self.undef_retval_nosym = False
         self.undefined_are_pure = False
         # link all that we have by default
-        self.linkundef = ['verifier', 'libc', 'posix', 'kernel']
+        self.linkundef = ['svcomp', 'verifier', 'libc', 'posix', 'kernel']
         self.timeout = 0
         self.add_libc = False
         self.no_lib = False
