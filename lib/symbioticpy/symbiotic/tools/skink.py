ec6582145bd17013a37574661636eb3b30479816
Marek Chalupa <xchalup4@fi.muni.cz>
2018-04-23 21:45:04 +0200

    skink: the file must be .ll, not .bc

--- lib/symbioticpy/symbiotic/tools/skink.py
+++ lib/symbioticpy/symbiotic/tools/skink.py
@@ -11,3 +11,13 @@ class SymbioticTool(SkinkTool):
     def llvm_version(self):
         return '5.0.1'
 
+    def preprocess_llvm(self, infile):
+        """
+        A tool's specific preprocessing steps for llvm file
+        before verification itself. Returns a pair (cmd, outputfile),
+        where cmd is the list suitable to pass to Popen and outputfile
+        is the resulting file from the preprocessing
+        """
+        output = infile + '.ll'
+        return (['llvm-dis', infile, '-o', output], output)
+
