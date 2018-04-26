7ac1af4facde9ee2b2c306e6bc07e5407aef6992
Marek Chalupa <xchalup4@fi.muni.cz>
2018-04-26 16:47:53 +0200

    skink: use compiler options as in skink invocation on C

--- lib/symbioticpy/symbiotic/tools/skink.py
+++ lib/symbioticpy/symbiotic/tools/skink.py
@@ -8,6 +8,9 @@ class SymbioticTool(SkinkTool):
 
     REQUIRED_PATHS = SkinkTool.REQUIRED_PATHS
 
+    def __init__(self, opts):
+        self._options = opts
+
     def llvm_version(self):
         return '5.0.1'
 
@@ -20,4 +23,23 @@ class SymbioticTool(SkinkTool):
         """
         output = infile + '.ll'
         return (['llvm-dis', infile, '-o', output], output)
+
+    def compilation_options(self):
+        """
+    List of compilation options specific for this tool
+    """
+                #'-target','i386-pc-linux-gnu','-m32','-O2',
+        opts = ['-Wno-implicit-function-declaration','-Wno-incompatible-library-redeclaration','-fno-vectorize',
+                '-fno-slp-vectorize','-gline-tables-only','-Xclang','-disable-lifetime-markers','-Rpass=.*','-Rpass-missed=.*',
+                '-Rpass-analysis=.*','-mllvm','-inline-threshold=15000','-Dassert=__VERIFIER_assert']
+        if self._options.property.undefinedness():
+                opts.append('-fsanitize=undefined')
+                opts.append('-fno-sanitize=unsigned-integer-overflow')
+        elif self._options.property.signedoverflow():
+                opts.append('-fsanitize=signed-integer-overflow')
+                opts.append('-fsanitize=shift')
+
+        return opts
+
+
 
