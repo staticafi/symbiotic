f27f3789a682c40b95b88d6014ebd01fd7fac2d2
Marek Chalupa <xchalup4@fi.muni.cz>
2018-06-27 12:09:25 +0200

    Mark possibly invalid free with mark_free instead of mark_pointer

--- lib/symbioticpy/symbiotic/tools/klee.py
+++ lib/symbioticpy/symbiotic/tools/klee.py
@@ -89,7 +89,7 @@ class SymbioticTool(KleeBase):
         if self._options.property.memsafety():
             # default config file is 'config.json'
             # slice with respect to the memory handling operations
-            return ('__INSTR_mark_pointer', ['-criteria-are-next-instr'])
+            return ('__INSTR_mark_pointer,__INSTR_mark_free', ['-criteria-are-next-instr'])
 
         return (self._options.slicing_criterion,[])
 
