7802dfc65488cdf9fed8a43c6079c7b5d4c07840
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-23 10:50:57 +0100

    Add a test

---
+++ tests/simple-true-valid-memsafety-false-valid-memcleanup.c
@@ -0,0 +1,18 @@
+int main(void) {
+	int *x = malloc(4);
+	__VERIFIER_error();
+	return 0;
+}
+/*
+int main(void) {
+	int size = 10;//intput();
+	int array[size];
+//	for (int i = 0; i < sizeof(int)*size; ++i)
+//		*((unsigned char *)array + i) = 0;
+	for (int i = 0; i < size + 0; ++i)
+		array[i] = 0;
+
+	while(1) {}
+	return 0;
+}
+*/
