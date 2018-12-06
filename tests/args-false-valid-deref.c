456ef9d62875d38c311a8f3fa4c60966e97f1ea1
Marek Chalupa <xchalup4@fi.muni.cz>
2018-12-06 15:02:10 +0100

    tests: add tests for bug in argc/argv[] handling
    Tests, thanks to @dkozovsk. Fix is not available yet.
    This belongs to issue #103.

---
+++ tests/args-false-valid-deref.c
@@ -0,0 +1,11 @@
+/* code by @dkozovsk */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+   if (argc == 2)
+      printf("%s\n",argv[5]);
+   return 0;
+}
+
---
+++ tests/args-true-valid-memsafety.c
@@ -0,0 +1,10 @@
+/* code by @dkozovsk */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n",argv[0]);
+	return 0;
+}
