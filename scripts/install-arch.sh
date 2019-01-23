c1b3c26a151ba25600c61959b1ef912bd59817ef
Marek Chalupa <mchqwerty@gmail.com>
2019-01-23 12:23:47 +0100

    scripts: add scripts for installing packages
    If the user wants...

---
+++ scripts/install-arch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# install clang if there is not suitable compiler
+if ! which g++ &>/dev/null; then
+	if ! which clang++ &>/dev/null; then
+		pacman -S install clang
+	fi
+fi
+
+
+pacman -S curl wget rsync make cmake unzip lib32-gcc-libs
