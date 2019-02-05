6ea764658350182a7dd6279fbb975ab923595e00
Marek Chalupa <xchalup4@fi.muni.cz>
2019-02-05 08:47:25 -0500

    move parsing options and environment into options.py and environment.py
    This is a temporary (I hope) solution, we'll move to argparse
    and completely rework parsing the options.

--- lib/symbioticpy/symbiotic/environment.py
+++ lib/symbioticpy/symbiotic/environment.py
@@ -1,6 +1,73 @@
 #!/usr/bin/python
 
 from os import environ
+from os.path import isfile, isdir
+
+def _set_symbiotic_environ(tool, env, opts):
+    if opts.search_include_paths:
+        from . includepaths import IncludePathsSearcher
+        additional_include_paths = IncludePathsSearcher().get()
+        for p in additional_include_paths:
+            env.prepend('C_INCLUDE_DIR', p)
+
+    # check whether we are in distribution directory or in the developement directory
+    if isfile('{0}/build.sh'.format(env.symbiotic_dir)):
+        opts.devel_mode = True
+    else:
+        opts.devel_mode = False
+
+    llvm_version = tool.llvm_version()
+    llvm_prefix = '{0}/llvm-{1}'.format(env.symbiotic_dir, llvm_version)
+
+    if not isdir(llvm_prefix):
+        err("Directory with LLVM binaries does not exist: '{0}'".format(llvm_prefix))
+
+    env.prepend('LD_LIBRARY_PATH', '{0}/lib'.format(env.symbiotic_dir))
+    env.prepend('LD_LIBRARY_PATH', '{0}/lib'.format(llvm_prefix))
+
+    env.prepend('C_INCLUDE_DIR', '{0}/include'.format(env.symbiotic_dir))
+    env.prepend('C_INCLUDE_DIR', '{0}/include'.format(llvm_prefix))
+    env.prepend('PATH', '{0}/llvm-{1}/bin:{0}/bin'.format(env.symbiotic_dir, llvm_version))
+
+    if opts.devel_mode:
+        env.prepend('PATH', '{0}/llvm-{1}/build/bin:{0}/bin'.format(env.symbiotic_dir, llvm_version))
+        env.prepend('PATH', '{0}/dg/build-{1}/tools'.format(env.symbiotic_dir, llvm_version))
+        env.prepend('PATH', '{0}/sbt-slicer/build-{1}/src'.format(env.symbiotic_dir, llvm_version))
+        env.prepend('PATH', '{0}/sbt-instrumentation/build-{1}/src'.format(env.symbiotic_dir, llvm_version))
+
+        env.prepend('LD_LIBRARY_PATH', '{0}/transforms/build-{1}/'.format(env.symbiotic_dir,llvm_version))
+        env.prepend('LD_LIBRARY_PATH', '{0}/dg/build-{1}/lib'.format(env.symbiotic_dir, llvm_version))
+        env.prepend('LD_LIBRARY_PATH', '{0}/sbt-instrumentation/build-{1}/analyses'.format(env.symbiotic_dir, llvm_version))
+        env.prepend('LD_LIBRARY_PATH', '{0}/sbt-instrumentation/ra/build-{1}/'.format(env.symbiotic_dir, llvm_version))
+        opts.instrumentation_files_path = '{0}/sbt-instrumentation/instrumentations/'.format(env.symbiotic_dir)
+    else:
+        opts.instrumentation_files_path = '{0}/share/sbt-instrumentation/'.format(llvm_prefix)
+    # Get include paths again now when we have our clang in the path,
+    # so that we have at least includes from our clang's instalation
+    # (these has the lowest prefs., so just append them
+    if opts.search_include_paths:
+        additional_include_paths = IncludePathsSearcher().get()
+        for p in additional_include_paths:
+            env.append('C_INCLUDE_DIR', p)
+
+    # let the tool set its specific environment
+    if hasattr(tool, 'set_environment'):
+        tool.set_environment(env, opts)
+
+def _parse_environ_vars(opts):
+    """
+    Parse environment variables of interest and
+    change running options accordingly
+    """
+    # FIXME: do not store these flags into opts but into environ
+    if 'C_INCLUDE_DIR' in environ:
+        for p in environ['C_INCLUDE_DIR'].split(':'):
+            if p != '':
+                opts.CPPFLAGS.append('-I{0}'.format(p))
+    if 'CFLAGS' in environ:
+        opts.CFLAGS += environ['CFLAGS'].split(' ')
+    if 'CPPFLAGS' in environ:
+        opts.CPPFLAGS += environ['CPPFLAGS'].split(' ')
 
 class Environment:
     """
@@ -29,4 +96,7 @@ class Environment:
 
         environ[env] = newenv
 
+    def set(self, tool, opts):
+        _set_symbiotic_environ(tool, self, opts)
+        _parse_environ_vars(opts)
 
