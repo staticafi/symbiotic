20e394bed70a6670b638374e3bee5059a1126f46
Marek Chalupa <xchalup4@fi.muni.cz>
2019-03-18 12:26:50 +0100

    Fix klee options
    klee recently replaced -disable-opt with --optimize=false.

--- lib/symbioticpy/symbiotic/tools/klee.py
+++ lib/symbioticpy/symbiotic/tools/klee.py
@@ -125,7 +125,7 @@ class SymbioticTool(KleeBase):
 
         cmd = [executable, '-write-paths',
                '-dump-states-on-halt=0', '-silent-klee-assume=1',
-               '-output-stats=0', '-disable-opt', '-only-output-states-covering-new=1',
+               '-output-stats=0', '--optimize=false', '-only-output-states-covering-new=1',
                '-max-time={0}'.format(self._options.timeout),
                '-external-calls=none']
         if self._options.property.memsafety():
