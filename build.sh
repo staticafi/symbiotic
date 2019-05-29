5d3ebe938f0d57145f289a0c5043e70e8d4b927d
Marek Chalupa <chalupa@fi.muni.cz>
2019-05-29 16:31:13 +0200

    bump the default version of LLVM to 8.0.0

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ usage()
 	echo -e "OPTS = options for make (i. e. -j8)"
 }
 
-LLVM_VERSION_DEFAULT=4.0.1
+LLVM_VERSION_DEFAULT=8.0.0
 get_llvm_version()
 {
 	# check whether we have llvm already present
