19b3b295f2e32e87a48e7aa3acad487caa86982f
Marek Chalupa <mchqwerty@gmail.com>
2019-06-18 15:50:15 +0200

    fix build scripts
    Missing " in a previous commit.

--- system-build.sh
+++ system-build.sh
@@ -562,7 +562,7 @@ fi
 		$LLVM_PREFIX/lib/libRangeAnalysisPlugin.so \
 		$LLVM_PREFIX/lib/libCheckNSWPlugin.so \
 		$LLVM_PREFIX/lib/libInfiniteLoopsPlugin.so \
-		$LLVM_PREFIX/lib/libValueRelationsPlugin.so
+		$LLVM_PREFIX/lib/libValueRelationsPlugin.so"
 
 if [ ${BUILD_KLEE} = "yes" ];  then
 	LIBRARIES="${LIBRARIES} \
