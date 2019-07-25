1c29301b0ef689e7f4e34f4acdecd58a32c2691d
Marek Chalupa <mchqwerty@gmail.com>
2019-07-25 08:29:06 +0000

    install scripts: ask about the installation of LLVM and Z3 on Fedora
    It may not be desired to install them automatically if the user plans
    to build them.

--- scripts/install-fedora.sh
+++ scripts/install-fedora.sh
@@ -2,12 +2,35 @@
 
 set -e
 
+PACKAGES="curl wget rsync make cmake unzip tar patch glibc-devel.i686 xz"
+
 # install clang if there is not suitable compiler
 if ! which g++ &>/dev/null; then
 	if ! which clang++ &>/dev/null; then
-		dnf install clang
+		PACKAGES="$PACKAGES clang"
 	fi
 fi
 
+INSTALL_Z3="N"
+INSTALL_LLVM="N"
 
-dnf install curl wget rsync make cmake unzip tar patch glibc-devel.i686 xz
+# Ask for these as the user may have his/her own build
+if ! rpm -qa | grep -q z3-devel; then
+	echo "Z3 not found, should I install it? [y/N]"
+	read INSTALL_Z3
+fi
+
+if ! rpm -qa | grep -q llvm-devel; then
+	echo "LLVM not found, should I install it? [y/N]"
+	read INSTALL_LLVM
+fi
+
+if [ "$INSTALL_Z3" = "y" ]; then
+	PACKAGES="$PACKAGES z3-devel"
+fi
+if [ "$INSTALL_LLVM" = "y" ]; then
+	PACKAGES="$PACKAGES llvm-devel"
+fi
+
+dnf install $PACKAGES
+
