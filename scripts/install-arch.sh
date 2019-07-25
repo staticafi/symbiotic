5b2868603fa6158165a9d2f9930f39433af9a33d
Marek Chalupa <mchqwerty@gmail.com>
2019-07-25 08:33:36 +0000

    install scripts: ask about LLVM and Z3 also for Arch
    See the previous commit.

--- scripts/install-arch.sh
+++ scripts/install-arch.sh
@@ -2,12 +2,34 @@
 
 set -e
 
+PACKAGES="curl wget rsync make cmake unzip lib32-gcc-libs xz"
+
 # install clang if there is not suitable compiler
 if ! which g++ &>/dev/null; then
 	if ! which clang++ &>/dev/null; then
-		pacman -S install clang
+		PACKAGES="$PACKAGES clang"
 	fi
 fi
 
+INSTALL_Z3="N"
+INSTALL_LLVM="N"
 
-pacman -S curl wget rsync make cmake unzip lib32-gcc-libs xz
+# Ask for these as the user may have his/her own build
+if ! pacman -Qqe | grep -q z3; then
+	echo "Z3 not found, should I install it? [y/N]"
+	read INSTALL_Z3
+fi
+
+if ! pacman -Qqe | grep -q llvm; then
+	echo "LLVM not found, should I install it? [y/N]"
+	read INSTALL_LLVM
+fi
+
+if [ "$INSTALL_Z3" = "y" ]; then
+	PACKAGES="$PACKAGES z3"
+fi
+if [ "$INSTALL_LLVM" = "y" ]; then
+	PACKAGES="$PACKAGES llvm"
+fi
+
+pacman -S $PACKAGES
