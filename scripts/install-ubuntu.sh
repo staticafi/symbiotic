1e69e8776339104def06f6f6c69987fddae09294
Marek Chalupa <mchqwerty@gmail.com>
2019-07-25 08:37:53 +0000

    install scripts: ask for LLVM and Z3 on Ubuntu
    See the prev-prev commit.

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -4,11 +4,34 @@ set -e
 
 apt-get update
 
+PACKAGES="curl wget rsync make cmake unzip gcc-multilib xz-utils"
+
 # install clang if there is not suitable compiler
 if ! which g++ &>/dev/null; then
 	if ! which clang++ &>/dev/null; then
-		apt-get install clang
+		PACKAGES="$PACKAGES clang"
 	fi
 fi
 
-apt-get install curl wget rsync make cmake unzip gcc-multilib xz-utils
+INSTALL_Z3="N"
+INSTALL_LLVM="N"
+
+# Ask for these as the user may have his/her own build
+if !  dpkg -l | grep -q libz3-dev; then
+	echo "Z3 not found, should I install it? [y/N]"
+	read INSTALL_Z3
+fi
+
+if !  dpkg -l | grep -q 'llvm.*-dev'; then
+	echo "LLVM not found, should I install it? [y/N]"
+	read INSTALL_LLVM
+fi
+
+if [ "$INSTALL_Z3" = "y" ]; then
+	PACKAGES="$PACKAGES z3"
+fi
+if [ "$INSTALL_LLVM" = "y" ]; then
+	PACKAGES="$PACKAGES llvm"
+fi
+
+apt-get install $PACKAGES
