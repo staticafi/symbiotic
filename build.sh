63cd846c05d5868623e979acc678055f316c736a
Marek Chalupa <chalupa@fi.muni.cz>
2019-09-23 16:27:12 +0200

    build: incorporate llvm2c

--- build.sh
+++ build.sh
@@ -793,6 +793,41 @@ if [ "`pwd`" != $ABS_SRCDIR ]; then
 fi
 
 ######################################################################
+#   llvm2c
+######################################################################
+if [ $FROM -le 6 ]; then
+	# initialize instrumentation module if not done yet
+	if [  "x$UPDATE" = "x1" -o -z "$(ls -A $SRCDIR/llvm2c)" ]; then
+		git_submodule_init
+	fi
+
+	pushd "$SRCDIR/llvm2c" || exitmsg "Cloning failed"
+	mkdir -p build-${LLVM_VERSION}
+	pushd build-${LLVM_VERSION}
+	if [ ! -d CMakeFiles ]; then
+		cmake .. \
+			-DCMAKE_BUILD_TYPE=${BUILD_TYPE}\
+			-DCMAKE_INSTALL_LIBDIR:PATH=lib \
+			-DCMAKE_INSTALL_FULL_DATADIR:PATH=$LLVM_PREFIX/share \
+			-DLLVM_SRC_PATH="$LLVM_SRC_PATH" \
+			-DLLVM_BUILD_PATH="$LLVM_BUILD_PATH" \
+			-DLLVM_DIR=$LLVM_DIR \
+			-DCMAKE_INSTALL_PREFIX=$LLVM_PREFIX \
+			|| clean_and_exit 1 "git"
+	fi
+
+	(build && make install) || exit 1
+
+	popd
+	popd
+fi
+
+if [ "`pwd`" != $ABS_SRCDIR ]; then
+	exitmsg "Inconsistency in the build script, should be in $ABS_SRCDIR"
+fi
+
+
+######################################################################
 #   instrumentation
 ######################################################################
 if [ $FROM -le 6 ]; then
@@ -1001,6 +1036,7 @@ get_klee_dependencies()
 
 	BINARIES="$LLVM_PREFIX/bin/sbt-slicer \
 		  $LLVM_PREFIX/bin/llvm-slicer \
+		  $LLVM_PREFIX/bin/llvm2c \
 		  $LLVM_PREFIX/bin/sbt-instr"
 	for B in $LLVM_TOOLS; do
 		BINARIES="$LLVM_PREFIX/bin/${B} $BINARIES"
