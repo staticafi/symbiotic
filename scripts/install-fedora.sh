5c4de69dba455e1573cea1957bdb61df1d45d7c6
Marek Chalupa <chalupa@fi.muni.cz>
2019-11-11 15:26:05 +0100

    Update modules
    For KLEE, we also have to install libsqlite in build scripts,
    since it is newly dependent on it.

--- scripts/install-fedora.sh
+++ scripts/install-fedora.sh
@@ -13,6 +13,7 @@ fi
 
 INSTALL_Z3="N"
 INSTALL_LLVM="N"
+INSTALL_SQLITE="N"
 
 # Ask for these as the user may have his/her own build
 if ! rpm -qa | grep -q z3-devel; then
@@ -24,6 +25,10 @@ if ! rpm -qa | grep -q llvm-devel; then
 	echo "LLVM not found, should I install it? [y/N]"
 	read INSTALL_LLVM
 fi
+if ! rpm -qa | grep -q libsqlite3-devel; then
+	echo "SQLite not found, should I install it? [y/N]"
+	read INSTALL_SQLITE
+fi
 
 if [ "$INSTALL_Z3" = "y" ]; then
 	PACKAGES="$PACKAGES z3-devel"
@@ -31,6 +36,11 @@ fi
 if [ "$INSTALL_LLVM" = "y" ]; then
 	PACKAGES="$PACKAGES llvm-devel llvm-static"
 fi
+if [ "$INSTALL_SQLITE" = "y" ]; then
+	PACKAGES="$PACKAGES libsqlite3-devel"
+fi
+
+
 
 dnf install $PACKAGES
 
