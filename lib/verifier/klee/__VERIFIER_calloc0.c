1317809a451b0af9e06409e0e9ee578d685f06eb
Marek Chalupa <mchqwerty@gmail.com>
2019-11-25 07:00:24 +0100

    build: precompile also models of functions
    The same way as the instrumentation files.

--- lib/verifier/klee/__VERIFIER_calloc0.c
+++ lib/verifier/klee/__VERIFIER_calloc0.c
@@ -4,13 +4,13 @@
 
 extern void *malloc(size_t);
 extern void *memset(void *s, int c, size_t n);
-extern void klee_make_nondet(void *, size_t, const char *, int);
+extern void klee_make_symbolic(void *, size_t, const char *);
 
-void *__VERIFIER_calloc0(size_t nmem, size_t size, int id)
+void *__VERIFIER_calloc0(size_t nmem, size_t size)
 {
 	void *mem = malloc(nmem * size);
 	//klee_assume(mem != (void *) 0);
-	klee_make_nondet(mem, nmem * size, "--:calloc0:", id);
+	klee_make_symbolic(mem, nmem * size, "calloc");
 	memset(mem, 0, nmem * size);
 
 	return mem;
--- lib/verifier/klee/__VERIFIER_malloc.c
+++ lib/verifier/klee/__VERIFIER_malloc.c
@@ -2,17 +2,17 @@
 
 extern void *malloc(size_t);
 extern _Bool __VERIFIER_nondet__Bool(void);
-extern void klee_make_nondet(void *, size_t, const char *, int);
+extern void klee_make_symbolic(void *, size_t, const char *);
 
 /* add our own versions of malloc and calloc */
 /* non-deterministically return memory or NULL */
-void *__VERIFIER_malloc(size_t size, int id)
+void *__VERIFIER_malloc(size_t size)
 {
 	if (__VERIFIER_nondet__Bool())
 		return ((void *) 0);
 
 	void *mem = malloc(size);
-	klee_make_nondet(mem, size, "--:malloc:", id);
+	klee_make_symbolic(mem, size, "malloc");
 
 	return mem;
 }
