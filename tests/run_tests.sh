f38eb7630956b475ba938acc5e32314c1eefdb9c
Marek Chalupa <mchqwerty@gmail.com>
2019-12-17 14:19:11 +0100

    tests: fix running tests after last changes
    We must use --report=sv-comp (for benchexec) and we also
    want to run the devel version of symbiotic, i.e., from scripts/
    and without integrity check.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-export PATH=$(pwd)/../install/bin/:$PATH
+# run symbiotic from the scripts/ preferably, so that the tests use
+# the current (developement) version
+export PATH=$(pwd)/../scripts:$(pwd)/../install/bin/:$PATH
+export ASAN_OPTIONS=detect_leaks=0
 benchexec --read-only-dir / --read-only-dir /home --full-access-dir $(pwd)/.. --container symbiotic-tests.xml $@
