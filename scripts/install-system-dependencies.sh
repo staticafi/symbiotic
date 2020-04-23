3babcf9350031a6c8b23ca686f377deb328ca953
root <mchqwerty@gmail.com>
2020-04-23 06:02:56 +0000

    install-system-dependencies: fix for missing sudo
    Use su if there is no sudo.

--- scripts/install-system-dependencies.sh
+++ scripts/install-system-dependencies.sh
@@ -4,12 +4,20 @@ set -e
 
 cd $(dirname $0)
 
+SUDO=
+if [ which sudo &>/dev/null ]; then
+	SUDO=sudo
+else
+	echo "Need root privileges:"
+	SUDO="su -c"
+fi
+
 if which apt-get &>/dev/null; then
-	sudo ./install-ubuntu.sh
+	$SUDO ./install-ubuntu.sh
 elif which pacman &>/dev/null; then
-	sudo ./install-arch.sh
+	$SUDO ./install-arch.sh
 elif which dnf &>/dev/null; then
-	sudo ./install-fedora.sh
+	$SUDO ./install-fedora.sh
 elif which yum &>/dev/null; then
-	sudo ./install-fedora.sh
+	$SUDO ./install-fedora.sh
 fi
