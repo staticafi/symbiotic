bb701e5ea54c30d676d33eb4e6673c4643a01f46
Marek Chalupa <chalupa@fi.muni.cz>
2020-11-16 17:39:06 +0100

    libc: allow strdup to fail

--- lib/posix/strdup.c
+++ lib/posix/strdup.c
@@ -1,6 +1,6 @@
 #include "symbiotic-size_t.h"
 
-extern void *__VERIFIER_malloc(size_t);
+extern _Bool __VERIFIER_nondet__Bool(void);
 extern void *malloc(size_t);
 extern void *memcpy(void *dest, const void *src, size_t n);
 extern size_t strlen(const char*);
@@ -8,6 +8,8 @@ extern size_t strlen(const char*);
 char *strdup(const char *str)
 {
 	size_t len = strlen(str) + 1;
+	if (__VERIFIER_nondet__Bool())
+		return ((void *) 0);
 	char *mem = malloc(len);//__VERIFIER_malloc(len);
 	if (!mem)
 		return ((void *) 0);
