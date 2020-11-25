165c992983e365e060b53961e3a1417a32c99e13
Marek Chalupa <mchqwerty@gmail.com>
2020-11-25 18:04:34 +0100

    Reduce the warnings written to output
    Write some of the warnings only once.

--- lib/libc/svcomp/strtod.c
+++ lib/libc/svcomp/strtod.c
@@ -2,11 +2,11 @@
 
 extern double __VERIFIER_nondet_double(void);
 extern int __VERIFIER_nondet_int(void);
-extern void klee_warning(const char *);
+extern void klee_warning_once(const char *);
 
 double strtod(const char *str, char **endptr) {
 	(void)str;
-	klee_warning("overapproximating function model");
+	klee_warning_once("overapproximating function model");
 	if (endptr) // check the dereference
 		*endptr = str;
 	errno = __VERIFIER_nondet_int();
