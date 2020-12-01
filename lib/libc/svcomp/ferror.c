1ae9a688422299e1a1e1e4c589cd082045712056
Marek Chalupa <mchwqerty@gmail.com>
2020-12-01 09:12:39 +0100

    Tweak function models

--- lib/libc/svcomp/ferror.c
+++ lib/libc/svcomp/ferror.c
@@ -1,11 +1,10 @@
+
 extern void klee_warning_once(const char *);
 void klee_silent_exit(int) __attribute__((noreturn));
 
-unsigned long strtoul(const char *nptr, char **endptr, int base)
-{
-    *nptr;
-    if (endptr)
-        *endptr;
+int ferror(void* f) {
 	klee_warning_once("unsupported function model");
-    klee_silent_exit(1);
+    	klee_silent_exit(1);
 }
+
+
--- lib/libc/svcomp/strtoul.c
+++ lib/libc/svcomp/strtoul.c
@@ -1,13 +1,12 @@
-#include <time.h>
 
 extern void klee_warning_once(const char *);
 void klee_silent_exit(int) __attribute__((noreturn));
 
-size_t strftime(char *restrict s, size_t n, const char *restrict f, const struct tm *restrict tm)
-{
-	*s;
-	*f;
-	*tm;
+unsigned long int strtoul(const char *nptr, char **endptr, int base) {
+	*nptr;
+	if (endptr)
+		*endptr;
+	// otherwise we do not know how to do that properly...
 	klee_warning_once("unsupported function model");
     	klee_silent_exit(1);
 }
