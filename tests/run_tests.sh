e829a2ddf7fa58e8650f81bf746a17439ff0323e
Lukas Zaoral <lzaoral@redhat.com>
2021-07-05 08:57:57 +0200

    tests: Use a custom test runner in place of benchexec

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,13 @@
 
 # run symbiotic from the scripts/ preferably, so that the tests use
 # the current (developement) version
-export PATH=$(pwd)/../scripts:$(pwd)/../install/bin/:$PATH
+export PATH=$PWD/../scripts:$PWD/../install/bin/:$PATH
 export ASAN_OPTIONS=detect_leaks=0
-benchexec --read-only-dir / --read-only-dir /home --full-access-dir $(pwd)/.. --container symbiotic-tests.xml $@
+
+# GitHub Actions defines this variable
+if [ -n "$CI" ]; then
+  color='--color'
+fi
+
+./test_runner.py "$color" ./*.set
+./test_runner.py "$color" --32 ./*.set
