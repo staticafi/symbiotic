7759802586a578b218bce6ba896589a908faeba2
Marek Chalupa <mchqwerty@gmail.com>
2021-08-27 16:05:09 +0200

    klee: model strtoull.c

---
+++ lib/libc/klee/strtoull.c
@@ -0,0 +1,12 @@
+
+extern void klee_warning_once(const char *);
+void klee_silent_exit(int) __attribute__((noreturn));
+
+unsigned long long int strtoull(const char *nptr, char **endptr, int base) {
+	*nptr;
+	if (endptr)
+		*endptr;
+	// otherwise we do not know how to do that properly...
+	klee_warning_once("unsupported function model");
+    	klee_silent_exit(1);
+}
