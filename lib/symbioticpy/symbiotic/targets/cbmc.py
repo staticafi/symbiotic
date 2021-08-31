da3568287f95aeba85f0d357e6451ced3bc3759a
Marek Chalupa <mchqwerty@gmail.com>
2021-08-31 17:28:04 +0200

    Change calling and parsing CBMC
    Assume mainstream CBMC, not SV-COMP CBMC.

--- lib/symbioticpy/symbiotic/targets/cbmc.py
+++ lib/symbioticpy/symbiotic/targets/cbmc.py
@@ -49,22 +49,18 @@ class SymbioticTool(BaseTool, SymbioticBaseTool):
     SV-COMP conditions is assumed.
     """
 
-    REQUIRED_PATHS = [
-                  "cbmc",
-                  "cbmc-binary",
-                  "goto-cc"
-                  ]
+    def __init__(self, opts):
+        SymbioticBaseTool.__init__(self, opts)
+        opts.explicit_symbolic = True
+
     def executable(self):
         return util.find_executable('cbmc')
-
 
     def version(self, executable):
         return self._version_from_tool(executable)
 
-
     def name(self):
         return 'CBMC'
-
 
     def cmdline(self, executable, options, tasks, propertyfile, rlimits):
        #if propertyfile:
@@ -72,94 +68,58 @@ class SymbioticTool(BaseTool, SymbioticBaseTool):
        #elif ("--xml-ui" not in options):
        #    options = options + ["--xml-ui"]
 
-        self.options = options
+        # parameters copied from the SV-COMP wrapper script
+        options.extend(['--stop-on-fail', '--object-bits', '11'])
+
+        prp = self._options.property
+        if prp.memsafety() or prp.memcleanup():
+            options.extend(['--pointer-check',
+                            '--memory-leak-check',
+                            '--bounds-check',
+                            '--no-assertions'])
+        elif prp.signedoverflow():
+            options.extend(['--signed-overflow-check',
+                            '--no-assertions'])
 
         return [executable] + options + tasks
 
-    def parse_XML(self, output, returncode, isTimeout):
-        #an empty tag cannot be parsed into a tree
-        def sanitizeXML(s):
-            return s.replace("<>", "<emptyTag>") \
-                    .replace("</>", "</emptyTag>")
-
-        try:
-            tree = ET.fromstringlist(map(sanitizeXML, output))
-            status = tree.findtext('cprover-status')
-
-            if status is None:
-                def isErrorMessage(msg):
-                    return msg.get('type', None) == 'ERROR'
-
-                messages = list(filter(isErrorMessage, tree.getiterator('message')))
-                if messages:
-                    # for now, use only the first error message if there are several
-                    msg = messages[0].findtext('text')
-                    if msg == 'Out of memory':
-                        status = 'OUT OF MEMORY'
-                    elif msg:
-                        status = 'ERROR ({0})'.format(msg)
-                    else:
-                        status = 'ERROR'
-                else:
-                    status = 'INVALID OUTPUT'
-
-            elif status == "FAILURE":
-                assert returncode == 10
-                reason = tree.find('goto_trace').find('failure').findtext('reason')
-                if not reason:
-                    reason = tree.find('goto_trace').find('failure').get('reason')
-                if 'unwinding assertion' in reason:
-                    status = result.RESULT_UNKNOWN
-                else:
-                    status = result.RESULT_FALSE_REACH
-
-            elif status == "SUCCESS":
-                assert returncode == 0
-                if "--unwinding-assertions" in self.options:
-                    status = result.RESULT_TRUE_PROP
-                else:
-                    status = result.RESULT_UNKNOWN
-
-        except Exception:
-            if isTimeout:
-                # in this case an exception is expected as the XML is invalid
-                status = 'TIMEOUT'
-            elif 'Minisat::OutOfMemoryException' in output:
-                status = 'OUT OF MEMORY'
-            else:
-                status = 'INVALID OUTPUT'
-                logging.exception("Error parsing CBMC output for returncode %d", returncode)
-
-        return status
-
-
-
     def determine_result(self, returncode, returnsignal, output, isTimeout):
-        output = list(output)
         if returnsignal == 0 and ((returncode == 0) or (returncode == 10)):
             status = result.RESULT_ERROR
-            if ('--xml-ui' in self.options):
-                status = self.parse_XML(output, returncode, isTimeout)
-            elif len(output) > 0:
-                # SV-COMP mode
-                result_str = output[-1].strip()
-                if result_str == b'TRUE' or result_str == b'VERIFICATION SUCCESSFUL':
-                    status = result.RESULT_TRUE_PROP
-                elif b'FALSE' in result_str:
-                    if result_str == b'FALSE(valid-memtrack)':
-                        status = result.RESULT_FALSE_MEMTRACK
-                    elif result_str == b'FALSE(valid-deref)':
-                        status = result.RESULT_FALSE_DEREF
-                    elif result_str == b'FALSE(valid-free)':
-                        status = result.RESULT_FALSE_FREE
-                    elif result_str == b'FALSE(no-overflow)':
-                        status = result.RESULT_FALSE_OVERFLOW
-                    elif result_str == b'FALSE(valid-memcleanup)':
-                        status = result.RESULT_FALSE_MEMCLEANUP
+            for line in output:
+                line = str(line.strip())
+                if "Unmodelled library functions have been called" in line:
+                    status = result.UNKNOWN
+                elif "__CPROVER_memory_leak" in line or\
+                     "allocated memory never freed" in line:
+                    status = result.RESULT_FALSE_MEMTRACK\
+                            if prp.memsafety() else result.RESULT_FALSE_MEMCLEANUP
+                elif "double free" in line or\
+                     "free called for stack-allocated object" in line or\
+                     "free argument" in line:
+                    status = result.RESULT_FALSE_FREE
+                elif "dereference failure" in line or\
+                     "bound in" in line or\
+                     "source region" in line:
+                    status = result.RESULT_FALSE_DEREF
+                elif "arithmetic overflow on signed" in line:
+                    status = result.RESULT_FALSE_OVERFLOW
+                elif "VERIFICATION SUCCESSFUL" in line:
+                    # sanity check
+                    if status != result.RESULT_ERROR:
+                        # we found some error and yet we were successful?
+                        status = 'PARSING FAILED'
                     else:
+                        status = result.RESULT_TRUE_PROP
+                elif "VERIFICATION FAILED" in line:
+                    if self._options.property.termination():
+                        status = result.RESULT_FALSE_TERMINATION
+                    elif self._options.property.unreachcall():
                         status = result.RESULT_FALSE_REACH
-                elif b'UNKNOWN' in output:
-                    status = result.RESULT_UNKNOWN
+                    # sanity check
+                    sw = status.lower().startswith
+                    if not (sw('false') or sw('unkown')):
+                        status = 'PARSING FAILED'
 
         elif returncode == 64 and 'Usage error!\n' in output:
             status = 'INVALID ARGUMENTS'
