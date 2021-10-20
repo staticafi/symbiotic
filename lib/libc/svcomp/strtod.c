812abd403bfd7952bd8dc190d9f206a496721daf
Marek Chalupa <mchqwerty@gmail.com>
2021-10-20 12:02:32 +0200

    svcomp: tweak the model of strtod

--- lib/libc/svcomp/strtod.c
+++ lib/libc/svcomp/strtod.c
@@ -1,15 +1,18 @@
 #include <errno.h>
 
-extern double __VERIFIER_nondet_double(void);
-extern int __VERIFIER_nondet_int(void);
+/** extern double __VERIFIER_nondet_double(void); */
+/** extern int __VERIFIER_nondet_int(void);       */
 extern void klee_warning_once(const char *);
+void klee_silent_exit(int) __attribute__((noreturn));
 
 double strtod(const char *str, char **endptr) {
-	(void)str;
-	klee_warning_once("overapproximating function model");
-	if (endptr) // check the dereference
-		*endptr = str;
-	errno = __VERIFIER_nondet_int();
-	return __VERIFIER_nondet_double();
+	klee_warning_once("unsupported function model");
+    klee_silent_exit(1);
+    // klee_warning_once("overapproximating function model");
+    // (void)str;
+    // if (endptr) // check the dereference
+    // 	*endptr = str;
+    // errno = __VERIFIER_nondet_int();
+    // return __VERIFIER_nondet_double();
 }
 
