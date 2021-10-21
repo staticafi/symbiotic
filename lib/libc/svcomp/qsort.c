0e878a50cc5d5bfcddbf808f1ba85e21b86fbf95
Marek Chalupa <mchqwerty@gmail.com>
2021-10-21 08:37:31 +0200

    lib: fix qsort
    There was a memory error for arrays of size 1.

--- lib/libc/svcomp/qsort.c
+++ lib/libc/svcomp/qsort.c
@@ -11,7 +11,7 @@ void klee_silent_exit(int) __attribute__((noreturn));
 void qsort(void *base, size_t nmemb, size_t size,
            int (*compar)(const void *, const void *)) {
     unsigned char *nxt = (unsigned char *) base;
-    for (unsigned i = 0; i < nmemb; ++i) {
+    for (unsigned i = 1; i < nmemb; ++i) {
         nxt += size;
         if (compar(base, nxt) > 0) { /* unsorted */
 	        klee_warning_once("unsorted array in qsort, aborting path");
