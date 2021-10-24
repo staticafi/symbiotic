5fd8935157878c07415175e4f38948725f3491d3
Lukas Zaoral <lzaoral@redhat.com>
2021-10-24 21:06:01 +0200

    fixup! tests: Use a custom test runner in place of benchexec
    cherrypick commit bebf0eefdd91dfa812c600 by @lzaoral.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 # run symbiotic from the scripts/ preferably, so that the tests use
-# the current (developement) version
-export PATH=$PWD/../scripts:$PWD/../install/bin/:$PATH
+# the current (development) version
+PATH="$PWD/../scripts:$PWD/../install/bin/:$PATH"
+
+# use llvm tools linked to symbiotic's install directory
+# FIXME: this is a really ugly hack
+LLVM_VERSION="$(symbiotic --version | grep 'LLVM' | cut -d' ' -f3)"
+export PATH="$PWD/../install/llvm-$LLVM_VERSION/bin:$PATH"
+
+# sanitizer settings
 export ASAN_OPTIONS=detect_leaks=0
 
 # GitHub Actions defines this variable
